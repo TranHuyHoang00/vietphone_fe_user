dced113b072122c48f5271a09caa3b60152c1f55
Hoang <tranhuyhoang23022001@gmail.com>
2024-03-08 09:36:45 +0700

    update

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -50,3 +50,22 @@ function onLoad() {
 }
 
 window.onload = onLoad;
+
+const scrollToTopButton =
+    document.getElementById('scroll-to-top');
+
+// Show button when user scrolls down
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 100) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}   
